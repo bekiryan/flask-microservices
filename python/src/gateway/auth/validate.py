f82af5840e940a486be13ad006f9ab11a1ceb59f
bekiryan <agasibekiryan@gmail.com>
2024-02-23 20:35:10 +0400

    Added auth and gateway microservices

---
+++ python/src/gateway/auth/validate.py
@@ -0,0 +1,19 @@
+import os
+import requests
+
+
+def token(request):
+    if "Authorization" in request.headers:
+        return None, ("missing creds", 401)
+
+    token = request.headers["Authorization"]
+
+    if not token:
+        return None, ("missing creds", 401)
+
+    response = request.post(f"http://{os.environ.get('AUTH_SVC_ADDRESS')}/validate", headers={"Authorization": token})
+
+    if response.status_code == 200:
+        return response.txt, None
+
+    return None, (response.txt, response.status_code)
---
+++ python/src/gateway/auth_svc/access.py
@@ -0,0 +1,15 @@
+import os
+import requests
+
+
+def login(request):
+    auth = request.authorization
+    if not auth:
+        return None, ("missing credentials", 401)
+    
+    basic_auth = auth.username, auth.password
+    response = request.post(f"http://{os.environ.get('AUTH_SVC_ADDRESS')}/login", auth=basic_auth)
+
+    if response.status_code == 200:
+        return response.txt, None
+    return None, (response.txt, response.status_code)
